514a4c700f1144916c87098f1067b907cef71d6e
MOHAMAD-ZUBI <mozubi128@hotmail.com>
2024-05-26 22:27:52 +0300

    lecturer profile half

--- shared/drawer.js
+++ shared/drawer.js
@@ -1,10 +1,26 @@
-import React, { useState, Component } from 'react';
-import { View, Text, TouchableOpacity,StyleSheet } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { Ionicons } from '@expo/vector-icons';
-import { MaterialIcons } from '@expo/vector-icons';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-const Drawer = ({isDrawerOpen, setIsDrawerOpen, isLecturer}) => {
+import React, { useState, Component } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import axios from "axios";
+import * as SecureStore from "expo-secure-store";
+
+const getRole = async () => {
+  const role = await SecureStore.getItemAsync("role");
+  return JSON.parse(role);
+};
+
+const Drawer = ({ isDrawerOpen, setIsDrawerOpen, isLecturer }) => {
+  const [role, setRole] = useState(null);
+
+  const handlerRole = async () => {
+    const role = await getRole();
+    if (role[0] === "Academician") setRole("Lecturer");
+  };
+  handlerRole();
   const navigation = useNavigation();
 
   const toggleDrawer = () => {
@@ -12,58 +28,62 @@ const Drawer = ({isDrawerOpen, setIsDrawerOpen, isLecturer}) => {
   };
 
   const handleLogOut = () => {
-    navigation.navigate('E-campus');
+    navigation.navigate("E-campus");
   };
 
   const handleDrawerItemPress = (screenIdentifier) => {
-  const screenName = isLecturer ? `${screenIdentifier}Lecturer` : screenIdentifier;
+    let isLec;
+    if (screenIdentifier === "Academician") {
+      isLec = "true";
+    }
+    const screenName = isLec ? `Lecturer${screenIdentifier}` : screenIdentifier;
     navigation.navigate(screenName);
     setIsDrawerOpen(false); // Close the drawer after navigating
   };
-
+  console.log(role);
   return (
     <View style={styless.container}>
-      
       <View style={styless.navBar}>
         <TouchableOpacity onPress={toggleDrawer} style={styless.menuButton}>
-          <Ionicons name={isDrawerOpen ? 'close' : 'menu'} size={35} color="white" />
+          <Ionicons
+            name={isDrawerOpen ? "close" : "menu"}
+            size={35}
+            color="white"
+          />
         </TouchableOpacity>
-        {isLecturer && (
-        <Text style={styless.title}>Lecturer Campus</Text>
+        {role === "Academician" && (
+          <Text style={styless.title}>Lecturer Campus</Text>
         )}
-        {!isLecturer && (
-        <Text style={styless.title}>E-Campus</Text>
-        )}
+        {role !== "Academician" && <Text style={styless.title}>E-Campus</Text>}
       </View>
 
-       
-    {/*  Drawer Content Goes Here */}
-    {isDrawerOpen && (
-        <View style={styless.drawer}>         
-          <TouchableOpacity onPress={() => handleDrawerItemPress('Profile')}>
+      {/*  Drawer Content Goes Here */}
+      {isDrawerOpen && (
+        <View style={styless.drawer}>
+          <TouchableOpacity
+            onPress={() => handleDrawerItemPress(`${role}Profile`)}
+          >
             <Text style={styless.drawerContent}>Profile</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => handleDrawerItemPress('Requests')} >
+          <TouchableOpacity onPress={() => handleDrawerItemPress("Requests")}>
             <Text style={styless.drawerContent}>Requests</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => handleDrawerItemPress('Courses')} >
+          <TouchableOpacity onPress={() => handleDrawerItemPress("Courses")}>
             <Text style={styless.drawerContent}>Courses</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => handleDrawerItemPress('SPgroups')} >
+          <TouchableOpacity onPress={() => handleDrawerItemPress("SPgroups")}>
             <Text style={styless.drawerContent}>Senior Project Groups</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => handleDrawerItemPress('Department')} >
+          <TouchableOpacity onPress={() => handleDrawerItemPress("Department")}>
             <Text style={styless.drawerContent}>Department</Text>
           </TouchableOpacity>
-        
+
           <TouchableOpacity style={styless.button} onPress={handleLogOut}>
             <Text style={styless.buttonText}>Log Out</Text>
           </TouchableOpacity>
-          
         </View>
- )}  
- 
+      )}
     </View>
   );
 };
@@ -71,57 +91,56 @@ const Drawer = ({isDrawerOpen, setIsDrawerOpen, isLecturer}) => {
 const styless = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-between',
+    justifyContent: "space-between",
   },
-  
+
   navBar: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#223F76',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#223F76",
     paddingHorizontal: 10,
-    padding: '5%'
+    padding: "5%",
   },
   menuButton: {
-    position: 'absolute',
-    left: 10
+    position: "absolute",
+    left: 10,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
-    color: '#FFFFFF'
+    fontWeight: "bold",
+    color: "#FFFFFF",
   },
   drawer: {
-    position:"absolute",
+    position: "absolute",
     top: 50,
     left: 0,
     right: 0,
     bottom: 0,
     padding: 50,
-    backgroundColor: '#223F76'
+    backgroundColor: "#223F76",
   },
   drawerContent: {
     fontSize: 25,
-    fontWeight: 'bold',
-    textAlign: 'left',
-    color: '#FFFFFF',
-    marginTop:40,
-
+    fontWeight: "bold",
+    textAlign: "left",
+    color: "#FFFFFF",
+    marginTop: 40,
   },
   button: {
-    backgroundColor: 'red',
+    backgroundColor: "red",
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 5,
-    alignSelf: 'center',
-    marginTop: '15%',
+    alignSelf: "center",
+    marginTop: "15%",
   },
   buttonText: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
     fontSize: 16,
-    textAlign: 'center'
-  }
+    textAlign: "center",
+  },
 });
 
 export default Drawer;
