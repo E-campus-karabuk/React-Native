a7e928501460c16e10081898582abfe5594bdb0b
MUHANNAD SALKINI <mohanad.salkini@gmail.com>
2024-06-01 19:21:43 +0300

    Refactored CourseDetailsLecturer component with updated imports, layout, and styling

--- screens/CourseDetails.js
+++ screens/CourseDetails.js
@@ -1,14 +1,23 @@
-import React, { useLayoutEffect, useState } from 'react';
-import {Linking, View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView,Image } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { NavigationContainer } from '@react-navigation/native';
-import { useNavigation } from '@react-navigation/native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { MaterialIcons } from '@expo/vector-icons';
-import { EvilIcons } from '@expo/vector-icons';
-import { Entypo } from '@expo/vector-icons';
-import  Drawer from '../shared/drawer';
-import  BottomNavBar from '../shared/bottomNavbar';
+import React, { useLayoutEffect, useState } from "react";
+import {
+  Linking,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+  Image,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { EvilIcons } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+import Drawer from "../shared/drawer";
+import BottomNavBar from "../shared/bottomNavbar";
 import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 
@@ -17,20 +26,20 @@ const getToken = async () => {
   return JSON.parse(token);
 };
 
-
-
-const CourseDetails = ({route}) => {
-    const navigation = useNavigation();
-    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-    const [token, setToken] = useState(null);
-    const [response, setResponse] = useState(null);
-    const { courseId, courseName, instructor } = route.params;
+const CourseDetails = ({ route }) => {
+  const navigation = useNavigation();
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [token, setToken] = useState(null);
+  const [response, setResponse] = useState(null);
+  const { courseId, courseName, instructor } = route.params;
   const goToLecturerProfile = () => {
-    navigation.navigate('LecturerProfile');
+    navigation.navigate("LecturerProfile");
   };
 
   const handlePress = (url) => {
-    Linking.openURL(url).catch(err => console.error("Failed to open URL: ", err));
+    Linking.openURL(url).catch((err) =>
+      console.error("Failed to open URL: ", err)
+    );
   };
 
   useLayoutEffect(() => {
@@ -50,8 +59,8 @@ const CourseDetails = ({route}) => {
               },
             }
           );
-           // console.log({data});
-           setResponse(data);
+          // console.log({data});
+          setResponse(data);
         }
       } catch (error) {
         console.log({ error: error.message });
@@ -61,97 +70,105 @@ const CourseDetails = ({route}) => {
     fetchTokenAndCourse();
   }, [token]);
 
-  
   return (
     <View style={styles.container}>
-    <Drawer
-    isDrawerOpen={isDrawerOpen}
-    setIsDrawerOpen={setIsDrawerOpen}
-    isLecturer={false}/>
+      <Drawer
+        isDrawerOpen={isDrawerOpen}
+        setIsDrawerOpen={setIsDrawerOpen}
+        isLecturer={false}
+      />
 
-{!isDrawerOpen && (
-<View style={styles.mainContent}>
-  
-<ScrollView>
-        <Text style={styles.heading}>{response?.courseCode} {response?.courseName}</Text>
-        
-        <TouchableOpacity >
-          <View style={styles.instructorRec}  >
-                <Image
-                  source={require('../assets/avatar-man-square.png')}
-                  style={styles.avatar}  
-                /> 
-                
-                <View style={styles.textContainer}>
-                  <Text style={styles.instructorRecText}>Dr. {response?.lecturer[0].firstName} {response?.lecturer[0].lastName}</Text>
-                  
-                </View>
-          </View>
-          </TouchableOpacity>
-          <Text style={styles.heading}>Notes & Related Links</Text>
-          
-           <View style={styles.tableHeader}>
-          
-          
-          </View>
-          <View style={styles.card}>
+      {!isDrawerOpen && (
+        <View style={styles.mainContent}>
           <ScrollView>
-              {response?.notes?.map((note) => {
-                return (
-                  <TouchableOpacity style={styles.smallcard} onPress={() => handlePress(`${process.env.EXPO_PUBLIC_API_URL}/${note.file}`)}>
-                    <Image
-                      source={require('../assets/pdf.png')}
-                      style={styles.fileTypeImg}  
-                    />
-                    <View style={styles.textContainerTable}>
-                      <Text style={styles.tableCardHeading}>{note.title}</Text>
-                      <Text style={styles.tableCardDate}>{new Date(note.createdAt).toLocaleDateString()}</Text>
-                    </View>
-                    <View style={styles.cardIcon}>
-                      <Entypo name="dots-three-horizontal" size={10} color="#1F3D75"/>
-                    </View>
-                  </TouchableOpacity>
-                );
-              })}
-              
-          </ScrollView>    
-          </View>
-          </ScrollView>    
+            <Text style={styles.heading}>
+              {response?.courseCode} {response?.courseName}
+            </Text>
+
+            <TouchableOpacity>
+              <View style={styles.instructorRec}>
+                <Image
+                  source={require("../assets/avatar-man-square.png")}
+                  style={styles.avatar}
+                />
+
+                <View style={styles.textContainer}>
+                  <Text style={styles.instructorRecText}>
+                    Dr. {response?.lecturer[0].firstName}{" "}
+                    {response?.lecturer[0].lastName}
+                  </Text>
                 </View>
+              </View>
+            </TouchableOpacity>
+            <Text style={styles.heading}>Notes & Related Links</Text>
 
-      )} 
+            <View style={styles.tableHeader}></View>
+            <View style={styles.card}>
+              <ScrollView>
+                {response?.notes?.map((note) => {
+                  return (
+                    <TouchableOpacity
+                      style={styles.smallcard}
+                      onPress={() =>
+                        handlePress(
+                          `${process.env.EXPO_PUBLIC_API_URL}/${note.file}`
+                        )
+                      }
+                    >
+                      <Image
+                        source={require("../assets/pdf.png")}
+                        style={styles.fileTypeImg}
+                      />
+                      <View style={styles.textContainerTable}>
+                        <Text style={styles.tableCardHeading}>
+                          {note.title}
+                        </Text>
+                        <Text style={styles.tableCardDate}>
+                          {new Date(note.createdAt).toLocaleDateString()}
+                        </Text>
+                      </View>
+                      <View style={styles.cardIcon}>
+                        <Entypo
+                          name="dots-three-horizontal"
+                          size={10}
+                          color="#1F3D75"
+                        />
+                      </View>
+                    </TouchableOpacity>
+                  );
+                })}
+              </ScrollView>
+            </View>
+          </ScrollView>
+        </View>
+      )}
       {!isDrawerOpen && (
-      <TouchableOpacity style={styles.chatpot}>
-        <Ionicons name="help-circle-sharp" size={30} color="white" />
-      </TouchableOpacity>
-      )} 
+        <TouchableOpacity style={styles.chatpot}>
+          <Ionicons name="help-circle-sharp" size={30} color="white" />
+        </TouchableOpacity>
+      )}
 
-
-      {!isDrawerOpen && (
-        <BottomNavBar 
-        isLecturer={false}/>
-      )} 
-      </View>
-      );
-      };
-
+      {!isDrawerOpen && <BottomNavBar isLecturer={false} />}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'#FFFFFF'
+    backgroundColor: "#FFFFFF",
   },
   mainContent: {
     zIndex: 0,
-    height: '82%',
-    left:0,
-    right:0
+    height: "82%",
+    left: 0,
+    right: 0,
   },
- navBar: {
-    flexDirection: 'row',
-    alignItems: 'center',
+  navBar: {
+    flexDirection: "row",
+    alignItems: "center",
     height: 50,
-    backgroundColor: '#223F76',
+    backgroundColor: "#223F76",
     paddingHorizontal: 10,
     zIndex: 100,
   },
@@ -160,255 +177,250 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#FFFFFF'
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#FFFFFF",
   },
 
   drawer: {
-    position: 'absolute',
-  top: 0,
-  left: 0,
-  bottom: 0,
-  right: 0,
-  backgroundColor: '#223F76',
-  padding: 50,
-  zIndex: 99,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    backgroundColor: "#223F76",
+    padding: 50,
+    zIndex: 99,
   },
   drawerContent: {
     fontSize: 25,
-    fontWeight: 'bold',
-    textAlign: 'left',
-    color: '#FFFFFF',
-    marginBottom:10,
-    marginTop:30
+    fontWeight: "bold",
+    textAlign: "left",
+    color: "#FFFFFF",
+    marginBottom: 10,
+    marginTop: 30,
   },
   button: {
-    backgroundColor: '#C8272E',
+    backgroundColor: "#C8272E",
     paddingVertical: 10,
     paddingHorizontal: 20,
-    width:148,
+    width: 148,
     borderRadius: 5,
-    marginLeft:'25%',
-    marginTop:74
+    marginLeft: "25%",
+    marginTop: 74,
   },
   buttonText: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
     fontSize: 16,
-    textAlign: 'center'
+    textAlign: "center",
   },
-  
-  bottomNavBar:{
-    position: 'absolute',
+
+  bottomNavBar: {
+    position: "absolute",
     bottom: 0,
     left: 0,
     right: 0,
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     height: 80,
-    backgroundColor: '#223F76',
-    paddingLeft:10,
-    paddingRight:60,
-    paddingBottom:20,
-    
-   },
-   
-   bottomNavBarContent:{
-   
-    textAlign: 'center',
-    color: '#FFFFFF',
-    marginHorizontal:'16%'
-   },
-   
+    backgroundColor: "#223F76",
+    paddingLeft: 10,
+    paddingRight: 60,
+    paddingBottom: 20,
+  },
+
+  bottomNavBarContent: {
+    textAlign: "center",
+    color: "#FFFFFF",
+    marginHorizontal: "16%",
+  },
 
   chatpot: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 20,
     right: 20,
-    
-    backgroundColor: '#223F76', 
+
+    backgroundColor: "#223F76",
     borderRadius: 30,
     width: 60,
     height: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-    shadowColor: 'black',
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "black",
     shadowOpacity: 0.5,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 2,
     elevation: 3,
-    marginBottom:80
+    marginBottom: 80,
   },
   heading: {
     fontSize: 25,
-    fontWeight: 'bold',
-    textAlign: 'left',
+    fontWeight: "bold",
+    textAlign: "left",
     marginTop: 10,
     marginLeft: 10,
     padding: 20,
-    color: '#223F76'
+    color: "#223F76",
   },
   describtion: {
     fontSize: 15,
-    textAlign: 'left',
-    marginHorizontal:'7.5%',
-    marginTop:2,
-    color: '#C8272E'
+    textAlign: "left",
+    marginHorizontal: "7.5%",
+    marginTop: 2,
+    color: "#C8272E",
   },
   instructorRec: {
-    flexDirection: 'row', 
+    flexDirection: "row",
     height: 50,
-    backgroundColor: '#223F76',
+    backgroundColor: "#223F76",
     width: 240,
     borderRadius: 4,
     marginTop: 30,
-    marginLeft: '7.5%',
+    marginLeft: "7.5%",
   },
   textContainer: {
-    marginLeft: 10, 
-    justifyContent: 'center', 
+    marginLeft: 10,
+    justifyContent: "center",
   },
   instructorRecText: {
     fontSize: 12,
-    fontWeight: 'bold',
-    color: '#FFFFFF',
+    fontWeight: "bold",
+    color: "#FFFFFF",
   },
   viewProfileText: {
     marginTop: 5,
     fontSize: 10,
-    color: '#FFFFFF',
+    color: "#FFFFFF",
   },
   avatar: {
-    width: 50, 
-    height: 50, 
-    alignSelf: 'center', 
-    borderRadius:4,
+    width: 50,
+    height: 50,
+    alignSelf: "center",
+    borderRadius: 4,
   },
   heading: {
     fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'left',
+    fontWeight: "bold",
+    textAlign: "left",
     marginTop: 10,
     marginLeft: 10,
     padding: 20,
-    color: '#223F76'
+    color: "#223F76",
   },
-  tableHeader:{
-    flexDirection: 'row',
-    alignItems: 'center',
+  tableHeader: {
+    flexDirection: "row",
+    alignItems: "center",
     width: 360,
     height: 37,
     borderRadius: 4,
     borderWidth: 1,
-    borderColor: 'rgba(245, 245, 245, 0.32)',
-    backgroundColor: 'rgba(210, 210, 210, 0.20)',
-    marginLeft:16,
-    borderWidth: 2, 
-    borderColor:'#E8E8E8'
+    borderColor: "rgba(245, 245, 245, 0.32)",
+    backgroundColor: "rgba(210, 210, 210, 0.20)",
+    marginLeft: 16,
+    borderWidth: 2,
+    borderColor: "#E8E8E8",
   },
-  searchBar:{
-    flexDirection: 'row',
+  searchBar: {
+    flexDirection: "row",
     width: 121,
     height: 19,
     borderRadius: 4,
-    backgroundColor: 'rgba(210, 210, 210, 0.60)', 
-    padding:2
+    backgroundColor: "rgba(210, 210, 210, 0.60)",
+    padding: 2,
   },
   searchText: {
     fontSize: 12,
-    textAlign: 'left',
-    marginLeft:7,
-    marginRight:'45%',
-    marginBottom:2,
-    color: '#ffffff'
+    textAlign: "left",
+    marginLeft: 7,
+    marginRight: "45%",
+    marginBottom: 2,
+    color: "#ffffff",
   },
   category: {
     fontSize: 10,
-    textAlign: 'left',
-    marginHorizontal:'3%',
-    marginTop:2,
-    color: '#C8272E'
+    textAlign: "left",
+    marginHorizontal: "3%",
+    marginTop: 2,
+    color: "#C8272E",
   },
-  whiteFilterButton:{
-    flexDirection: 'row',
+  whiteFilterButton: {
+    flexDirection: "row",
     width: 60,
     height: 19,
     borderRadius: 4,
-    backgroundColor: '#ffffff', 
-    padding:2,
-    marginLeft:5,
-    alignItems: 'center',
+    backgroundColor: "#ffffff",
+    padding: 2,
+    marginLeft: 5,
+    alignItems: "center",
   },
   whiteFilterButtonText: {
     fontSize: 10,
-    textAlign: 'center',
-    marginLeft:10,
-    marginRight:3,
-    color: '#C8272E'
+    textAlign: "center",
+    marginLeft: 10,
+    marginRight: 3,
+    color: "#C8272E",
   },
   card: {
     width: 360,
     height: 322,
-    backgroundColor: 'rgba(245, 245, 245, 0.1)',
+    backgroundColor: "rgba(245, 245, 245, 0.1)",
     borderRadius: 4,
-    shadowOpacity:50,
-    shadowOffset:60,
-    shadowColor:'#E8E8E8',
+    shadowOpacity: 50,
+    shadowOffset: 60,
+    shadowColor: "#E8E8E8",
     marginLeft: 16,
-    borderWidth: 2, 
-    borderColor:'#E8E8E8',
-    marginBottom:'40%',
-    paddingVertical:15
+    borderWidth: 2,
+    borderColor: "#E8E8E8",
+    marginBottom: "40%",
+    paddingVertical: 15,
   },
-  smallcard: {  
-    flexDirection:'row',
+  smallcard: {
+    flexDirection: "row",
     width: 340,
     height: 86,
-    backgroundColor: '#FFFFFF',
+    backgroundColor: "#FFFFFF",
     borderRadius: 4,
-    marginBottom:7,
-    marginLeft:8,
-    paddingHorizontal:5,
- },
- cardIcon: {
-  marginLeft: '20%', 
-  marginTop:20,
-  justifyContent: 'right', 
-},
-tableCardHeading: {
-  fontSize: 13,
-  fontWeight: 'bold',
-  textAlign: 'left',
-  marginLeft: '10%',
-  marginTop:20,
-  color: '#1F3D75'
-},
-textContainerTable:{
-  alignItems: 'left',
-},
-fileTypeImg: {
-  width: 57, 
-  height: 70, 
-  alignSelf: 'center', 
-},
-tableCardDate: {
-  fontSize: 9,
-  textAlign: 'left',
-  marginTop:15,
-  marginRight:'20%',
-  marginLeft: '10%',
-  color: '#1F3D75'
-},
-icon: {
-  marginLeft:'45%',
-},
-textInput: {
-  fontSize: 12,
-  marginLeft: '7%',
-},
-})
+    marginBottom: 7,
+    marginLeft: 8,
+    paddingHorizontal: 5,
+  },
+  cardIcon: {
+    marginLeft: "20%",
+    marginTop: 20,
+    justifyContent: "right",
+  },
+  tableCardHeading: {
+    fontSize: 13,
+    fontWeight: "bold",
+    textAlign: "left",
+    marginLeft: "10%",
+    marginTop: 20,
+    color: "#1F3D75",
+  },
+  textContainerTable: {
+    alignItems: "left",
+  },
+  fileTypeImg: {
+    width: 57,
+    height: 70,
+    alignSelf: "center",
+  },
+  tableCardDate: {
+    fontSize: 9,
+    textAlign: "left",
+    marginTop: 15,
+    marginRight: "20%",
+    marginLeft: "10%",
+    color: "#1F3D75",
+  },
+  icon: {
+    marginLeft: "45%",
+  },
+  textInput: {
+    fontSize: 12,
+    marginLeft: "7%",
+  },
+});
 
-export default CourseDetails; 
-
-
+export default CourseDetails;
