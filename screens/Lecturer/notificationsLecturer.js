747fde094de138d2d6717117d2225ecf71263c2b
MUHANNAD SALKINI <mohanad.salkini@gmail.com>
2024-06-02 21:27:00 +0300

    Added notification functionality to Lecturer screen, including API call to retrieve notifications and display of notification list with timestamps

--- screens/Lecturer/notificationsLecturer.js
+++ screens/Lecturer/notificationsLecturer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useLayoutEffect, useState } from "react";
 import { View, Text, Image, TouchableOpacity, ScrollView } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
@@ -7,11 +7,50 @@ import { MaterialIcons } from "@expo/vector-icons";
 import styles from "../../shared/styles";
 import Drawer from "../../shared/drawer";
 import BottomNavBar from "../../shared/bottomNavbar";
+import axios from "axios";
+import * as SecureStore from "expo-secure-store";
 
 // TODO: ADD THE NOTIFICATION FUNCTIONALITY
+
+const getToken = async () => {
+  const token = await SecureStore.getItemAsync("token");
+  return JSON.parse(token);
+};
+
 const NotificationsLecturer = () => {
   const navigation = useNavigation();
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const [token, setToken] = useState(null);
+  const [response, setResponse] = useState(null);
+
+  useLayoutEffect(() => {
+    const fetchTokenAndNotis = async () => {
+      try {
+        const token = await getToken();
+
+        setToken(token); // Store token in state
+
+        if (token) {
+          const { data } = await axios.get(
+            `${process.env.EXPO_PUBLIC_API_URL}/api/notification/`,
+            {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          //  console.log(data);
+          setResponse(data);
+        }
+      } catch (error) {
+        console.log({ error: error.message });
+      }
+    };
+
+    fetchTokenAndNotis();
+  }, [token]);
 
   const toggleDrawer = () => {
     setIsDrawerOpen(!isDrawerOpen);
@@ -26,6 +65,26 @@ const NotificationsLecturer = () => {
   };
   const handleBottomNavBar = (screenName) => {
     navigation.navigate(screenName);
+  };
+
+  const getTimeAgo = (createdAt) => {
+    const currentTime = new Date();
+    const notificationTime = new Date(createdAt);
+    const timeDifference = currentTime - notificationTime;
+    const seconds = Math.floor(timeDifference / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+
+    if (days > 0) {
+      return `${days}d ago`;
+    } else if (hours > 0) {
+      return `${hours}h ago`;
+    } else if (minutes > 0) {
+      return `${minutes}m ago`;
+    } else {
+      return `${seconds}s ago`;
+    }
   };
 
   return (
@@ -63,78 +122,35 @@ const NotificationsLecturer = () => {
           </View>
 
           <ScrollView>
-            <TouchableOpacity style={styles.smallcard}>
-              <Image
-                source={require("../../assets/profile-user.png")}
-                style={styles.ProfilePicIcon}
-              />
-              <View style={styles.textContainerTable}>
-                <Text style={styles.tableCardHeading}>Sender Name</Text>
-                <Text style={styles.tableCardMsgPreview}>
-                  Message Text preview...
-                </Text>
-              </View>
+            {response?.noti?.map((notification) => {
+              return (
+                <TouchableOpacity
+                  style={styles.smallcard}
+                  key={notification._id}
+                >
+                  <Image
+                    source={require("../../assets/profile-user.png")}
+                    style={styles.ProfilePicIcon}
+                  />
+                  <View style={styles.textContainerTable}>
+                    <Text style={styles.tableCardHeading}>
+                      {notification.title}
+                    </Text>
+                    <Text style={styles.tableCardMsgPreview}>
+                      {notification.content}..
+                    </Text>
+                  </View>
 
-              <View style={styles.textContainerTable}>
-                <View style={styles.cardIconTable}>
-                  <Text style={styles.notificationTime}>10h</Text>
-                </View>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.smallcard}>
-              <Image
-                source={require("../../assets/profile-user.png")}
-                style={styles.ProfilePicIcon}
-              />
-              <View style={styles.textContainerTable}>
-                <Text style={styles.tableCardHeading}>Sender Name</Text>
-                <Text style={styles.tableCardMsgPreview}>
-                  Message Text preview...
-                </Text>
-              </View>
-
-              <View style={styles.textContainerTable}>
-                <View style={styles.cardIconTable}>
-                  <Text style={styles.notificationTime}>10h</Text>
-                </View>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.smallcard}>
-              <Image
-                source={require("../../assets/profile-user.png")}
-                style={styles.ProfilePicIcon}
-              />
-              <View style={styles.textContainerTable}>
-                <Text style={styles.tableCardHeading}>Sender Name</Text>
-                <Text style={styles.tableCardMsgPreview}>
-                  Message Text preview...
-                </Text>
-              </View>
-
-              <View style={styles.textContainerTable}>
-                <View style={styles.cardIconTable}>
-                  <Text style={styles.notificationTime}>10h</Text>
-                </View>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.smallcard}>
-              <Image
-                source={require("../../assets/profile-user.png")}
-                style={styles.ProfilePicIcon}
-              />
-              <View style={styles.textContainerTable}>
-                <Text style={styles.tableCardHeading}>Sender Name</Text>
-                <Text style={styles.tableCardMsgPreview}>
-                  Message Text preview...
-                </Text>
-              </View>
-
-              <View style={styles.textContainerTable}>
-                <View style={styles.cardIconTable}>
-                  <Text style={styles.notificationTime}>10h</Text>
-                </View>
-              </View>
-            </TouchableOpacity>
+                  <View style={styles.textContainerTable}>
+                    <View style={styles.cardIconTable}>
+                      <Text style={styles.notificationTime}>
+                        {getTimeAgo(notification.createdAt)}
+                      </Text>
+                    </View>
+                  </View>
+                </TouchableOpacity>
+              );
+            })}
           </ScrollView>
         </View>
       )}
